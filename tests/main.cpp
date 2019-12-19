9f6a2534f540affc4b166af02908641b832d8334
Lancern <msrlancern@126.com>
2019-12-19 15:05:34 +0800

    Unit test boilerplate.

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Sirui Mu on 2019/12/19.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
